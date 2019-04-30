942a1ff416cea4ee6e7ffa0b760a5f3a564fd7c0
Chris Lincoln <chris.lincoln12@gmail.com>
2019-04-30 14:19:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,10 +6,10 @@ def display_board(board)
   puts "-----------"
   puts "   | #{board[4]} |   "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board = ["X","X","X"," "," "," "," "," "," "]
+board = [" "," "," "," "," "," ","O","O","O"]
 display_board(board)
 
 
